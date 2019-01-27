0eadc696d0e37046da32e85984dcf36e1ff795c5
bk7v <46638986+bk7v@users.noreply.github.com>
2019-01-27 13:53:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,6 +22,9 @@ const client = new Discord.Client({disableEveryone: true});
 
 const moment = require("moment");  
 
+const moment = require("table");  
+
+
 let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 
 
